fdbb82298781342c76f7d639acfc3a1982fc1b8d
Gina <it21410@hua.gr>
2019-03-21 23:32:16 +0200

    Added romanize folder

---
+++ DiplomaThesis/romanize/__main__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import sys
+import os
+
+path = os.path.dirname(sys.modules[__name__].__file__)
+path = os.path.join(path, '..')
+sys.path.insert(0, path)
+
+from . import romanize
+
+
+def main():
+    import sys
+
+    if len(sys.argv) > 1:
+        print(romanize(' '.join(sys.argv[1:])))
+    else:
+        print(sys.stdin.encoding)
+        for line in sys.stdin:
+            line = line.decode(sys.stdin.encoding)
+            # words = sys.stdin.read()
+            # encoding=sys.stdin.encoding
+            print(repr(line))
+            print(romanize(line))
+
+
+if __name__ == "__main__":
+    main()
