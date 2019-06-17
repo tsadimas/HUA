957d34b14fe3c806eec45c7bd03d991490592394
GeorgiaVlassi <44193919+GeorgiaVlassi@users.noreply.github.com>
2019-06-17 14:21:54 +0300

    Delete forms.py

--- DiplomaThesis/thesis/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from .models import ThesisApplication
-
-class ThesisApplicationForm(forms.ModelForm):
-    class Meta:
-        model = ThesisApplication
-        fields = ['semester', 'left_lessons']
-
