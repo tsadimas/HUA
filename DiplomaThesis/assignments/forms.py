c2601861d95139e3fcf315cc692d1b863394223e
GeorgiaVlassi <44193919+GeorgiaVlassi@users.noreply.github.com>
2019-06-20 22:59:38 +0300

    Delete forms.py

--- DiplomaThesis/assignments/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import AssignmentApplication
-
-
-class AssignmentApplicationForm(forms.ModelForm):
-    class Meta:
-        model = AssignmentApplication
-        fields = ['topic']
